5838cf7885d47fdba58d09c17a2b9f6043d22bf4
Karen Nikoghosyan <63981352+KarenNikoghosyan@users.noreply.github.com>
2020-09-13 20:51:51 +0300

    27 Weapon Zoom - Field Of View

---
+++ 6_Zombie_Runner/Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    Camera fpsCamera;
+    [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomedInFOV = 25f;
+    bool isZoom = false;
+    
+    void Start()
+    {
+        fpsCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            HandleZoom();
+        }
+    }
+
+    private void HandleZoom()
+    {
+        if (isZoom)
+        {
+            fpsCamera.fieldOfView = zoomedOutFOV;
+            isZoom = false;
+        }
+        else if (!isZoom)
+        {
+            fpsCamera.fieldOfView = zoomedInFOV;
+            isZoom = true;
+        }
+    }
+   
+}
