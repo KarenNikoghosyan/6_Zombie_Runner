07dd0252076dac2d54974139f3357fbd53c46f7c
Karen Nikoghosyan <63981352+KarenNikoghosyan@users.noreply.github.com>
2020-09-20 17:27:47 +0300

    36 Quick Bug Fix Challenge

--- 6_Zombie_Runner/Assets/Scripts/WeaponZoom.cs
+++ 6_Zombie_Runner/Assets/Scripts/WeaponZoom.cs
@@ -9,11 +9,10 @@ public class WeaponZoom : MonoBehaviour
     [SerializeField] RigidbodyFirstPersonController fpsController;
     [SerializeField] float zoomedOutFOV = 60f;
     [SerializeField] float zoomedInFOV = 25f;
-    bool isZoom = false;
-
     [SerializeField] float zoomOutSensitivity = 2f;
-
     [SerializeField] float zoomInSensitivity = 0.5f;
+
+    bool isZoom = false;
 
     void Start()
     {
@@ -28,22 +27,36 @@ public class WeaponZoom : MonoBehaviour
         }
     }
 
+    private void OnDisable()
+    {
+        ZoomOut();
+    }
+
     private void HandleZoom()
     {
         if (isZoom)
         {
-            isZoom = false;
-            fpsCamera.fieldOfView = zoomedOutFOV;
-            fpsController.mouseLook.XSensitivity = zoomOutSensitivity;
-            fpsController.mouseLook.YSensitivity = zoomOutSensitivity;
+            ZoomOut();
         }
         else if (!isZoom)
         {
-            isZoom = true;
-            fpsCamera.fieldOfView = zoomedInFOV;
-            fpsController.mouseLook.XSensitivity = zoomInSensitivity;
-            fpsController.mouseLook.YSensitivity = zoomInSensitivity;
+            ZoomIn();
         }
     }
-   
+
+
+    private void ZoomOut()
+    {
+        isZoom = false;
+        fpsCamera.fieldOfView = zoomedOutFOV;
+        fpsController.mouseLook.XSensitivity = zoomOutSensitivity;
+        fpsController.mouseLook.YSensitivity = zoomOutSensitivity;
+    }
+    private void ZoomIn()
+    {
+        isZoom = true;
+        fpsCamera.fieldOfView = zoomedInFOV;
+        fpsController.mouseLook.XSensitivity = zoomInSensitivity;
+        fpsController.mouseLook.YSensitivity = zoomInSensitivity;
+    }
 }
