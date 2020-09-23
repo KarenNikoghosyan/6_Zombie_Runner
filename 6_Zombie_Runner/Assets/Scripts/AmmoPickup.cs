21aabcaa5740e7b91034609bc5017fffcf6b6e49
Karen Nikoghosyan <63981352+KarenNikoghosyan@users.noreply.github.com>
2020-09-23 16:21:11 +0300

    37 Ammo Pickup - Part 1

---
+++ 6_Zombie_Runner/Assets/Scripts/AmmoPickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            print("You have collected some ammo");
+            Destroy(gameObject);
+        }
+    }
+}
